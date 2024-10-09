74e29d71c3b6420f09a052f52ef8d9d9d807ec46
SchweGELBin <abramjannikmichael06@gmail.com>
2024-10-09 18:55:51 +0200

    feat: download checksum
    also added missing functionality from the old repo
    
    chore: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/schwegelbin/openbible/MainActivity.kt
+++ app/src/main/java/com/schwegelbin/openbible/MainActivity.kt
@@ -21,9 +21,31 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.schwegelbin.openbible.ui.theme.OpenBibleTheme
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import java.io.File
+
+@Serializable
+data class Verse(
+    val verse: String,
+    val text: String
+)
+
+@Serializable
+data class Chapter(
+    val name: String,
+    val verses: List<Verse>
+)
+
+@Serializable
+data class Book(val chapters: List<Chapter>)
+
+@Serializable
+data class Bible(val books: List<Book>)
 
 enum class Screen(val title: String) {
     Home("Home"),
@@ -53,8 +75,10 @@ fun MainApp() {
 @Composable
 fun App() {
     val context = LocalContext.current
-    if (!File(context.getExternalFilesDir(""), "translations.json").exists()) {
+    val path = context.getExternalFilesDir("")
+    if (!File("${path}/translations.json").exists() || !File("${path}/checksum.json").exists()) {
         saveIndex(context)
+        saveChecksum(context)
     }
 
     val currentScreen = remember { mutableStateOf(Screen.Home) }
@@ -69,7 +93,7 @@ fun App() {
                     colors = TopAppBarDefaults.topAppBarColors(
                       titleContentColor = MaterialTheme.colorScheme.primary
                     ),
-                    title = { Text("OpenBible") },
+                    title = { Text(stringResource(R.string.app_name)) },
                     actions = {
                         IconButton(onClick = { showSettings.value = true }) {
                             Icon(
@@ -122,7 +146,14 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun ReadScreen(modifier: Modifier = Modifier) {
-    Text(text = "Read Screen", modifier = modifier)
+    Text(text = "Read Screen".toString(), modifier = modifier)
+    /*
+    val context = LocalContext.current
+    Column {
+        Text(getChapter(context = context, abbrev = "schlachter", book = 18, chapter = 118).toString())
+        Text(getChapter(context = context, abbrev = "schlachter", book = 18, chapter = 118).toString())
+    }
+    */
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -180,26 +211,80 @@ private fun saveIndex(context: Context) {
     downloadFile(
         context = context,
         url = "https://api.getbible.net/v2/translations.json",
-        relPath = "",
         name = "translations.json"
     )
 }
 
-private fun downloadTranslation(context: Context, abbrev: String) {
+private fun saveChecksum(context: Context) {
+    val dir = context.getExternalFilesDir("")
+    val path = "${dir}/checksum.json"
+    File(path).delete()
     downloadFile(
         context = context,
-        url = String.format("https://api.getbible.net/v2/%s.json", abbrev),
-        relPath = "Translations",
-        name = String.format("%s.json", abbrev)
+        url = "https://api.getbible.net/v2/checksum.json",
+        name = "checksum.json"
     )
 }
 
-private fun downloadFile(context: Context, relPath: String, url: String, name: String) {
+private fun getChecksum(context: Context, abbrev: String): String? {
+    val dir = context.getExternalFilesDir("")
+    var path = "${dir}/checksum.json"
+    var json = File(path).readText()
+    var obj = Json.decodeFromString<JsonObject>(json)
+    return obj[abbrev].toString()
+}
+
+private fun downloadTranslation(context: Context, abbrev: String) {
+    val dir = context.getExternalFilesDir("")
+    var path = "${dir}/${abbrev}.json"
+    File(path).delete()
+    downloadFile(
+        context = context,
+        url = String.format("https://api.getbible.net/v2/%s.json", abbrev),
+        name = String.format("%s.json", abbrev)
+    )
+    path = "${dir}/${abbrev}.sum"
+    var checksum = getChecksum(context, abbrev).toString()
+    File(path).writeText(checksum)
+}
+
+private fun checkUpdate(context: Context, abbrev: String): Boolean {
+    val dir = context.getExternalFilesDir("")
+    val path = "${dir}/${abbrev}.sum"
+    if (!File(path).exists()) return true
+    var latest = getChecksum(context, abbrev)
+    var current = File(path).readText()
+    return latest != current
+}
+
+private fun getChapter(context: Context, abbrev: String, book: Int, chapter: Int): String? {
+    val dir = context.getExternalFilesDir("")
+    val path = "${dir}/${abbrev}.json"
+    var text = ""
+    var json = File(path).readText()
+    var bible = Json.decodeFromString<Bible>(json)
+    val verses = bible.books[book].chapters[chapter].verses
+    verses.forEach { verse ->
+        text += "${verse.verse} ${verse.text}\n"
+    }
+    return text
+}
+
+private fun getTitle(context: Context, abbrev: String, book: Int, chapter: Int): String? {
+    val dir = context.getExternalFilesDir("")
+    val path = "${dir}/${abbrev}.json"
+    var json = File(path).readText()
+    var bible = Json.decodeFromString<Bible>(json)
+    val title = bible.books[book].chapters[chapter].name
+    return "${abbrev}\n${title}"
+}
+
+private fun downloadFile(context: Context, url: String, name: String) {
     val request = DownloadManager.Request(Uri.parse(url)).apply {
-        setTitle("Downloading File")
+        setTitle("Downloading $name")
         setDescription("Downloading $name")
         setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-        setDestinationInExternalFilesDir(context, relPath, name)
+        setDestinationInExternalFilesDir(context, "", name)
     }
     val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
     downloadManager.enqueue(request)
