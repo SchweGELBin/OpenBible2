fb42b39eb73c46737bce60c3cadf075b89e6f04e
SchweGELBin <abramjannikmichael06@gmail.com>
2024-10-23 19:06:38 +0200

    chore: release 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.schwegelbin.openbible"
         minSdk = 27
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
