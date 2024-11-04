711ac46c5c350898267321b597ab9dc89c5f6291
SchweGELBin <abramjannikmichael06@gmail.com>
2024-11-04 17:35:27 +0100

    chore: bump to 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.schwegelbin.openbible"
         minSdk = 27
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
