9519080e3e3e849c1fd4074ad46712bfd367308b
SchweGELBin <abramjannikmichael06@gmail.com>
2025-01-02 20:52:23 +0100

    fix: back button closes the app at Read screen

--- app/src/main/kotlin/com/schwegelbin/openbible/ui/screens/Main.kt
+++ app/src/main/kotlin/com/schwegelbin/openbible/ui/screens/Main.kt
@@ -30,34 +30,46 @@ fun App(onThemeChange: (Boolean?, Boolean?, Boolean?) -> Unit) {
     val context = LocalContext.current
     var update = false
     if (getCheckAtStartup(context)) update = checkForUpdates(context, false)
-
-    var startDestination: Any = Start
-    if (!getFirstLaunch(context) && !update) startDestination = Read
+    var startDestination = if (update || getFirstLaunch(context)) Start else Read
 
     val navController = rememberNavController()
     NavHost(navController, startDestination = startDestination) {
         composable<Read> {
-            ReadScreen(
-                onNavigateToSelection = { isSplitScreen ->
-                    navController.navigate(Selection(isSplitScreen))
-                },
+            ReadScreen(onNavigateToSelection = { isSplitScreen ->
+                navController.navigate(Selection(isSplitScreen))
+            },
                 onNavigateToSettings = { navController.navigate(Settings) },
-                onNavigateToStart = { navController.navigate(Start) }
-            )
+                onNavigateToStart = {
+                    navController.navigate(Start) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                })
         }
         composable<Selection> { backStackEntry ->
             val selection = backStackEntry.toRoute<Selection>()
             SelectionScreen(
-                onNavigateToRead = { navController.navigate(Read) },
-                selection.isSplitScreen
+                onNavigateToRead = {
+                    navController.navigate(Read) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                }, selection.isSplitScreen
             )
         }
         composable<Settings> {
             SettingsScreen(
-                onNavigateToRead = { navController.navigate(Read) },
-                onThemeChange = onThemeChange
+                onNavigateToRead = {
+                    navController.navigate(Read) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                }, onThemeChange = onThemeChange
             )
         }
-        composable<Start> { StartScreen(onNavigateToRead = { navController.navigate(Read) }) }
+        composable<Start> {
+            StartScreen(onNavigateToRead = {
+                navController.navigate(Read) {
+                    popUpTo(0) { inclusive = true }
+                }
+            })
+        }
     }
 }
