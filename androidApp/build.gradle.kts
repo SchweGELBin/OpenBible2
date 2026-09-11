63fb80022825ae465a501f8add2c54f701fd1d59
SchweGELBin <schwegelbin@gmail.com>
2026-09-11 14:24:16 +0200

    perf: use new android release optimization

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,12 +24,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            optimization {
+                enable = true
+            }
         }
     }
 
